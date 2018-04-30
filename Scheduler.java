f88e72bfea9a201cc58bb31d72b19f6ca5b6efeb
hyuan483 <hyuan483@gmail.com>
2018-04-30 00:10:33 -0400

    Add files via upload
    Added comments for scheduler and finished algorithm

--- Scheduler.java
+++ Scheduler.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
 public class Scheduler {
+	public static int[] firstTimeArr;
+	public static int[] secondTimeArr;
+	public static int maxPrice; 
+	
+
 	public static void main(String[] args) {
-		int[] firstTimeArr;
-		int[] secondTimeArr;
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("How many times do you need to call an Uber/Lyft today");
 		
 		int numTimes = sc.nextInt();
 		
-		for (int i = 0; i < numTimes - 1; i++) {
+		for (int i = 0; i < numTimes; i++) {
 			//keeping track of the first time so we can call these all easily because we can tell the i
 			System.out.println("input the first time you want to start looking");
 			firstTimeArr = new int[numTimes];
@@ -22,7 +25,17 @@ public class Scheduler {
 			secondTimeArr = new int[numTimes];
 			int secondTime = sc.nextInt();
 			secondTimeArr[i] = secondTime;
+			
+			
+			System.out.println("What is the max price drop you are waiting for");
+			maxPrice = sc.nextInt();
+			
+			
+			//keep making calls to the api until the times are up
+			//add the prices returned to the input[] algorithm
+			//We should call scheduler algorithm here with the price inputs 
 		}
 		sc.close();
+		
 	}
 }
---
+++ SchedulerAlgorithm.java
@@ -0,0 +1,20 @@
+
+public class SchedulerAlgorithm {
+
+	int[] input;
+	
+	public SchedulerAlgorithm(int[] inputs) {
+
+		input = inputs;
+		int prevPrice = 0;
+		for (int price: input) {
+			if (prevPrice != 0) {
+				if (price - prevPrice > Scheduler.maxPrice) {
+					//make a call to api
+				}
+			}
+			prevPrice = price;
+		}
+	}
+	
+}
