5ba43e16dbea0b7a2e20b00475b8841e0167e62e
hyuan483 <hyuan483@gmail.com>
2018-04-29 23:00:56 -0400

    Scheduler
    Updated so we can access the time arrays using Scheduler.ArrName

--- Scheduler.java
+++ Scheduler.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Scheduler {
 	public static void main(String[] args) {
+		int[] firstTimeArr;
+		int[] secondTimeArr;
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("How many times do you need to call an Uber/Lyft today");
@@ -11,13 +13,13 @@ public class Scheduler {
 		for (int i = 0; i < numTimes - 1; i++) {
 			//keeping track of the first time so we can call these all easily because we can tell the i
 			System.out.println("input the first time you want to start looking");
-			int[] firstTimeArr = new int[numTimes];
+			firstTimeArr = new int[numTimes];
 			int firstTime = sc.nextInt();
 			firstTimeArr[i] = firstTime;
 			
 			//keeping track of the end time 
 			System.out.println("input the second time you want to stop looking");
-			int [] secondTimeArr = new int[numTimes];
+			secondTimeArr = new int[numTimes];
 			int secondTime = sc.nextInt();
 			secondTimeArr[i] = secondTime;
 		}
